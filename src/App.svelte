cb2c447fb062229788657232642a516b8a209fac
Marco Gilles <gilles.marco06@gmail.com>
2021-02-01 01:51:02 +0100

    beginning of writing some content

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,75 @@
 <script>
-	let name = 'world';
+	import moment from "moment";
+	import Diploma from "./components/Diploma.svelte";
+	import { sortBy } from "lodash"
+
+	$: age = moment.duration(moment() - moment("1999-03-28")).years();
+
+	let diplomas = [
+		{
+			title: "High school Degree in sciences",
+			startYear: 2014,
+			endYear: 2017,
+			quickDesc: "",
+			frenchEquivalent: "Baccalauret Scientifique",
+			link: ""
+		},
+		{
+			title: "Two years degree diploma in IT",
+			startYear: 2017,
+			endYear: 2019,
+			quickDesc: "",
+			frenchEquivalent: "DUT Informatique",
+			link: ""
+		},
+		{
+			title: "Master of Business Informatics",
+			startYear: 2019,
+			endYear: 2022,
+			quickDesc: "",
+			frenchEquivalent: "MIAGE",
+			link: ""
+		}
+	]
 </script>
 
 <style type="text/scss">
-	$color: red;
-
-	h1 {
-		color: $color;
-	}
 </style>
 
-<h1>Hello {name}!</h1>
+
+<h1>Marco GILLES</h1>
+
+<section>
+	<h2>Presentation</h2>
+	<div>
+		<div>
+			<img src="./assets/marcogillesphoto.png" height="100%" width="100%"/>
+		</div>
+		<div>
+			<h3>FullStack Developer</h3>
+			<p>{age} years old student in apprenticeship</p>
+			<p>Currently studying for my Master of Business Informatics</p>
+		</div>
+	</div>
+</section>
+
+<section>
+	<h2>Professionnal Career</h2>
+	<h3>Current position</h3>
+	<p>Working as FullStack developer at <a href="https://www.himydata.com/fr/">Himydata</a></p>
+</section>
+
+<section>
+	<h2>Interesting projects</h2>
+</section>
+
+<section>
+	<h2>Academic Background</h2>
+	{#each sortBy(diplomas, "startYear", "dsc").reverse() as diploma}
+		<Diploma {...diploma} />
+	{/each}
+</section>
+
+<footer>
+	<a href="https://github.com/Gilles-Marco/CV">Source code is available here</a>
+</footer>
---
+++ src/components/Diploma.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let title = "";
+    export let startYear;
+    export let endYear;
+    export let quickDesc = "";
+    export let frenchEquivalent = "";
+    export let link = "";
+</script>
+<style type="text/scss">
+</style>
+
+<div>
+    <h3>{title}</h3>
+    <p>Period : {startYear}-{endYear}</p>
+    <p>{quickDesc}</p>
+    <p>{frenchEquivalent}</p>
+    <a href="{link}">Link here</a>
+</div>
